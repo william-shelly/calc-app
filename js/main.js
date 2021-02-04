573a9c684e1045459c810febb5572c2969416576
william-shelly <william-shelly@users.noreply.github.com>
2021-02-04 09:40:51 -0600

    Adding max width size and adding w-initial class. removing operatorAction style

--- js/main.js
+++ js/main.js
@@ -15,13 +15,7 @@ resetButton = document.querySelector('#reset-button');
 equalsButton.addEventListener('click', function(e) {
     if (x.value !== '' && x.value !== null && x.value !== undefined && y.value !== '' && y.value !== null && y.value !== undefined ) {
         console.log('TRUE');
-        console.log(x.value);
-        console.log(y.value);
-        resetForm(e);
         if(operatorAction.value === 'add') {
-            console.log('add');
-            console.log(x.value);
-            console.log(y.value);
             total.value = operator.addNumbers(x.value,y.value);
         } else if(operatorAction.value === 'subtract') {
             total.value = operator.subtractNumbers(x.value,y.value);
@@ -30,7 +24,7 @@ equalsButton.addEventListener('click', function(e) {
         } else if(operatorAction.value === 'divide') {
             total.value = operator.divideNumbers(x.value,y.value);
         } else {
-            console.log('ummm... what?');
+            console.log('FALSE... ummm... what?');
         }
     } else {
         console.log('FALSE');
@@ -39,32 +33,12 @@ equalsButton.addEventListener('click', function(e) {
     }
 });
 
-resetButton.addEventListener('click', function(e) {
-    resetForm(e);
+resetButton.addEventListener('click', function() {
+    resetForm();
 });
 
-function resetForm(e) {
+function resetForm() {
     messageArea.innerHTML = '';
     messageArea.classList.remove('text-danger','p-2');
+    x.focus();
 }
-
-/* function addNumbers(a,b) {
-    console.log(Number(a) + Number(b));
-    return Number(a) + Number(b);
-}; */
-
-/* function subtractNumbers(a,b) {
-    console.log(Number(a) - Number(b));
-    console.log('function');
-    return Number(a) - Number(b);
-}; */
-
-/* function multiplyNumbers(a,b) {
-    console.log(Number(a) * Number(b));
-    return Number(a) * Number(b);
-}; */
-
-/* function divideNumbers(a,b) {
-    console.log(Number(a) / Number(b));
-    return Number(a) / Number(b);
-}; */
